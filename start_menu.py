e8525c9be3bc29cc3529972b730b724eed5bd57d
Tim.Ozkan <tim@mac.com>
2024-10-11 15:25:32 +0300

    start menu update

--- start_menu.py
+++ start_menu.py
@@ -1,12 +1,15 @@
 # Opening Menu func
 
 def first_screen():
-    print('\n\nLutfen secimini yap')
+    print('\n\nLütfen seçimini yap')
     while True:
-        start_menu_selection = input('1 - Yeni Oyun\n2 - Eski Maceraya Don\n3 - Cikis Yap\n\n\n>>>')
-        try:
-            start_menu_selection == int(start_menu_selection)
-            break
-        except:
-            print("Dogru bir secim yaptigina emin misin? Bence tekrar dene!!")
-    return start_menu_selection
+        start_menu_selection = input('1 - Yeni Oyun\n2 - Eski Maceraya Dön\n3 - Çıkış Yap\n\n\n>>> ')
+        
+        if start_menu_selection.isdigit():  # Check if input is a number
+            selection = int(start_menu_selection)
+            if selection in [1, 2, 3]:  # Validate the selection
+                return selection
+            else:
+                print("Seçiminiz 1, 2 veya 3 olmalı. Lütfen tekrar deneyin.")
+        else:
+            print("Lütfen geçerli bir sayı girin.")
